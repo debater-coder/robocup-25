083f76e994bbbfbb39ef2c2710a65e2ae3a0f778
Narninja <joshua.campbell678904@gmail.com>
2025-05-04 15:51:52 +1000

    Update Robocup_strats.py
    not much

--- strats/Robocup_strats.py
+++ strats/Robocup_strats.py
@@ -1,32 +1,55 @@
 # KEY
-# _h means horizontal
-# _v means vertcal
-
-# VARIABLE KEY
-# _action gives (rotation,translation,translation_vel,angular_vel)
-# _pos gives (position_direction,distance,movement_vel)
-# _vel gives (vel_h,vel_v)
-# _dir gives (displacement_h,displacement_v)
-
-# ACTION
-#  receives an input of positioning
-
-# INPUT
-#  time_diff = int(x)
-#  self_action = (rotation,translation,translation_vel,angular_vel)
-#   self_rotation = angle the destination is
-#   self_translation = distance the destination
-#   self_translation_vel = (self_translation_vel_h,self_translation_vel_v)
-#    self_translation_vel_h = translation horizontal velocity of self
-#    self_translation_vel_v = translation vertical velocity of self
-#   self_angular_vel = (self_angular_vel_h,self_angular_vel_v)
-#    self_angular_vel_h = angle speed of turn
-#    self_angular_vel_v = 
-#  self_pos
-
-# OUTPUT
+# SUFFIX    MEANING                 STRUCTURE
+#
+# '_info'   information             (_action,_pos,_state)
+# '_diff'   difference              (_interger_)
+# '_action' describes the move
+# 
+# '_h'      horizontal component    
+# '_v'      vertcal component       
+# 
+# '_ang' means the angle (clockwise)
+# '_trans' means the translation distance
+# ''
+# '_pos' means position             (pos_ang,distance,movement_vel)
+# '_disp' means displacment 
 #
 
-def action(time_diff,self_action,self_pos,team_pos,opp1_pos,opp2_pos,ball_pos,state):
-    
+# VARIABLE KEY
+# _action gives (angle,trans,trans_vel,angular_vel)
+# _pos gives (pos_ang,distance,movement_vel)
+# _vel gives (vel_h,vel_v)
+# _dir gives (disp_h,disp_v)
+
+
+# INPUT
+#   time_diff = int(x)
+#   self_action = (angle,trans,trans_vel,angular_vel)
+#       self_ang
+#       self_trans
+#       self_trans_vel = (self_trans_vel_h,self_trans_vel_v)
+#           self_trans_vel_h
+#           self_trans_vel_v
+#       self_angular_vel = (self_angular_vel_h,self_angular_vel_v)
+#           self_angular_vel_h
+#           self_angular_vel_v 
+#       self_pos = (pos_ang,distance,movement_vel)
+#           self_pos_ang
+#       state = int(y)
+#   team_pos
+#       
+#   opp1_pos
+#       
+#   opp2_pos
+#       
+#   ball_pos
+#       
+#   
+
+# OUTPUT
+# drib_vel
+
+def action(time_diff, self_info, team_pos, opp1_pos, opp2_pos, ball_pos):
+    (self_action,self_pos,state) = self_info
+
     
