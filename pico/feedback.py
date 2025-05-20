f20c04943e90e5a60d1d1b6a3a8a760f61724db2
debater-coder <hamzah@syedahmed.net>
2025-05-20 15:11:58 +1000

    remove test code

---
+++ pico/feedback.py
@@ -0,0 +1,82 @@
+from machine import Pin
+from motor import Motor
+import time
+import micropython
+
+micropython.alloc_emergency_exception_buf(100)
+
+
+class Encoder:
+    """Encoder for motor. Uses only one encoder input as direction sensing was unreliable"""
+
+    def __init__(self, pin):
+        pin = Pin(pin, Pin.IN, Pin.PULL_UP)
+        self.pin = pin
+        self.odom = 0
+        self.direction = 1
+
+        self.pin.irq(self.irq_callback, Pin.IRQ_RISING)
+
+    def irq_callback(self, pin: Pin):
+        self.odom += self.direction
+
+    def __str__(self) -> str:
+        return str(self.odom)
+
+
+
+class MotorFeedback:
+    """Motor using encoder as speed feedback. Direction is assumed from motor direction."""
+
+    def __init__(
+        self,
+        pwm: int,
+        dir: int,
+        encoder: int,
+        pulses_per_revolution: int = 1050,
+        kp: float = 0.1,
+        ki: float = 0,
+        kd: float = 0,
+        reverse=False,
+    ):
+        self.motor = Motor(pwm, dir)
+        self.encoder = Encoder(encoder)
+        self.ppm = pulses_per_revolution
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.reverse = reverse
+        self.speed = 0 # rev/s
+        self.target = 0 # rev/s
+        self.motor_power = 0 # [-100, 100]
+        self.last_time = time.ticks_us()
+        self.last_error = 0
+        self.odom = 0
+
+    def update(self):
+        displacement = self.encoder.odom
+        self.odom += displacement
+
+        # calculate speed
+        elapsed = time.ticks_diff(time.ticks_us(), self.last_time)
+        self.speed = (displacement / self.ppm) / (elapsed / 1000000)
+
+        # reset odom and time
+        self.encoder.odom = 0
+        self.last_time = time.ticks_us()
+
+        # PID
+        error = self.target - self.speed
+
+        proportional = self.kp * error
+
+        control = proportional
+        self.motor_power += control
+        self.motor_power = max(-100, min(100, self.motor_power))
+
+        # update motor
+        self.motor.speed(self.motor_power * (-1 if self.reverse else 1))
+        self.encoder.direction = round(self.motor_power / abs(self.motor_power))
+
+    def set_speed(self, speed):
+        self.target = speed
--- pico/kinematics.py
+++ pico/kinematics.py
@@ -6,112 +6,17 @@ https://ecam-eurobot.github.io/Tutorials/mechanical/mecanum.html
 # Components
 inverse kinematics: determine motor speeds necessary for desired movement
 PID control loop: use encoder input to keep motor speeds close to calculated speeds
-forwards kinematics: use encoder input to determine current chassis movement
+forward kinematics (pose estimator): use encoder input to determine current chassis movement
 odometry: integrate forward kinematics results over time to obtain displacement (odom transform)
 """
+from feedback import MotorFeedback
 
-from machine import Pin
-from Makerverse_Motor_2ch import Motor
-import time
-import micropython
+# stage 1: accurate movement forwards only
+# stage 2: four directions
+# stage 3: four directions + rotation
+# stage 4: pose based movement
 
-micropython.alloc_emergency_exception_buf(100)
-
-
-class Encoder:
-    """Encoder for motor. Uses only one encoder input as direction sensing was unreliable"""
-
-    def __init__(self, pin):
-        pin = Pin(pin, Pin.IN, Pin.PULL_UP)
-        self.pin = pin
-        self.odom = 0
-        self.direction = 1
-
-        self.pin.irq(self.irq_callback, Pin.IRQ_RISING)
-
-    def irq_callback(self, pin: Pin):
-        self.odom += self.direction
-
-    def __str__(self) -> str:
-        return str(self.odom)
-
-
-
-class MotorFeedback:
-    """Motor using encoder as speed feedback. Direction is assumed from motor direction."""
-
-    def __init__(
-        self,
-        pwm: int,
-        dir: int,
-        encoder: int,
-        pulses_per_revolution: int = 1050,
-        kp: float = 0.1,
-        ki: float = 0,
-        kd: float = 0,
-        reverse=False,
-    ):
-        self.motor = Motor(pwm, dir)
-        self.encoder = Encoder(encoder)
-        self.ppm = pulses_per_revolution
-        self.kp = kp
-        self.ki = ki
-        self.kd = kd
-        self.reverse = reverse
-        self.speed = 0 # rev/s
-        self.target = 0 # rev/s
-        self.motor_power = 0 # [-100, 100]
-        self.last_time = time.ticks_us()
-        self.last_error = 0
-
-    def update(self):
-        displacement = self.encoder.odom
-
-        # calculate speed
-        elapsed = time.ticks_diff(time.ticks_us(), self.last_time)
-        self.speed = (displacement / self.ppm) / (elapsed / 1000000)
-
-        # reset odom and time
-        self.encoder.odom = 0
-        self.last_time = time.ticks_us()
-
-        # PID
-        error = self.target - self.speed
-
-        proportional = self.kp * error
-
-        control = proportional
-        self.motor_power += control
-        self.motor_power = max(-100, min(100, self.motor_power))
-
-        # update motor
-        self.motor.speed(self.motor_power * (-1 if self.reverse else 1))
-        self.encoder.direction = round(self.motor_power / abs(self.motor_power))
-
-    def set_speed(self, speed):
-        self.target = speed
-
-
-rl = MotorFeedback(6, 7, 11, reverse=True) # done
-rl.set_speed(0.5)
-
-fl = MotorFeedback(4, 5, 19, reverse=True) # done
-fl.set_speed(0.5)
-
-rr = MotorFeedback(8, 9, 13) # done
-rr.set_speed(0.5)
-
-fr = MotorFeedback(2, 3, 21) # done
-fr.set_speed(0.5)
-
-try:
-    while True:
-        rl.update()
-        fl.update()
-        rr.update()
-        fr.update()
-except:
-    rl.motor.stop()
-    fl.motor.stop()
-    rr.motor.stop()
-    fr.motor.stop()
+rl = MotorFeedback(6, 7, 11, reverse=True)
+fl = MotorFeedback(4, 5, 19, reverse=True)
+rr = MotorFeedback(8, 9, 13)
+fr = MotorFeedback(2, 3, 21)
