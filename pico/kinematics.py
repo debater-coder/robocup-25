92c467838203d03b4e855ce18b2aa7c2def65d54
debater-coder <hamzah@syedahmed.net>
2025-07-29 14:04:21 +1000

    fix: calibration constants for odometry

--- pico/kinematics.py
+++ pico/kinematics.py
@@ -5,16 +5,20 @@ https://ecam-eurobot.github.io/Tutorials/mechanical/mecanum.html
 
 # Components
 inverse kinematics: determine motor speeds necessary for desired movement
-PID control loop: use encoder input to keep motor speeds close to calculated speeds
+PID control loop: use encoder input to keep motor speeds close to calculated speeds [TODO]
 forward kinematics (pose estimator): use encoder input to determine current chassis movement
 odometry: integrate forward kinematics results over time to obtain displacement (odom transform)
 """
 from feedback import MotorFeedback
-import time
+import math
 
 LX = 0.05  # half-length X between wheels
 LY = 0.05  # half-length Y between wheels
 R = 0.024  # radius of wheels
+
+CALIBRATION_X = 1.98 / 0.3256874
+CALIBRATION_Y = 1.03 / 0.2719314
+CALIBRATION_W = 6.28 / 1.23
 
 def inverse_kinematics(vx: float, vy: float, w: float):
     rotation = (LX + LY) * w
@@ -41,18 +45,16 @@ class Kinematics:
         self.fr = MotorFeedback(2, 3, 21)
         self.motors = [self.fl, self.fr, self.rl, self.rr]
         self.odom = (0, 0, 0)  # x (m), y (m), w (radians)
-        self.last_time = time.ticks_us()
 
     def update(self):
         for motor in self.motors:
             motor.update()
 
-        elapsed = time.ticks_diff(time.ticks_us(), self.last_time) * 1e6
-        vel = forwards_kinematics(*(motor.odom for motor in self.motors))
+        disp = forwards_kinematics(*(motor.odom for motor in self.motors))
+        for motor in self.motors:
+            motor.odom = 0
 
-        self.last_time = time.ticks_us()  # reset timer
-
-        self.odom = tuple((odom + elapsed * component for odom, component in zip(self.odom, vel)))
+        self.odom = (self.odom[0] + disp[0] * CALIBRATION_X, self.odom[1] + disp[1] * CALIBRATION_Y, self.odom[2] + disp[2] * CALIBRATION_W)
         print(" ".join(map(str, self.odom)))
 
     def set_speed(self, vx: float, vy: float, w: float):
