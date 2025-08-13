a50d5ecdcc721a673c5ac02fb38d5a8e5364b9e4
debater-coder <hamzah@syedahmed.net>
2025-08-13 16:42:17 +1000

    feat: /pose websocket

--- controller/behaviours/localisation.py
+++ controller/behaviours/localisation.py
@@ -15,30 +15,27 @@ class Localisation(py_trees.behaviour.Behaviour):
         self.blackboard.register_key(
             "odom", access=py_trees.common.Access.READ, required=True
         )
-
-        self.posetree: PoseTree | None = None
+        self.blackboard.register_key("posetree", access=py_trees.common.Access.WRITE)
 
     def setup(self, **kwargs: typing.Any) -> None:
         """Used to dependency inject PoseTree instance."""
         if "posetree" in kwargs and isinstance(
             posetree := kwargs["posetree"], PoseTree
         ):
-            self.posetree = posetree
+            self.blackboard.posetree = posetree
         else:
             raise TypeError("Expected posetree to be passed in Localisation.setup()")
 
     def update(self) -> py_trees.common.Status:
         """Stub implementation that just converts odometry into a pose"""
-        if not self.posetree:
-            raise ValueError("Expected Localisation.setup() to be called.")
 
         pose = Pose.from_position_and_rotation(
             [self.blackboard.odom[0], self.blackboard.odom[1], 0],
             Rotation.from_euler("z", self.blackboard.odom[2]),
             "field",
-            self.posetree,
+            self.blackboard.posetree,
         )
 
-        self.posetree.add_frame(pose, "chassis")
+        self.blackboard.posetree.add_frame(pose, "chassis")
 
         return py_trees.common.Status.RUNNING
--- controller/components/debug_server.py
+++ controller/components/debug_server.py
@@ -1,10 +1,23 @@
 from __future__ import annotations
-from aiohttp import web
+from aiohttp import web, WSMsgType
 import aiohttp_cors
 from multiprocessing import Queue
+import asyncio
+
+import posetree
 
 
-def debug_sever(target_vel_queue: Queue[tuple[float, float, float]]):
+def debug_sever(
+    target_vel_queue: Queue[tuple[float, float, float]],
+    current_pose: Queue[tuple[float, float, float]],
+):
+    """
+    Debug server running in a different process. Uses queues for communication.
+
+    Arguments:
+    target_vel_queue: Queue of (vx, vy, vw) written to from this function
+    current_pose: Queue of (x, y, w) read from this function
+    """
     routes = web.RouteTableDef()
 
     @routes.post("/target_vel")
@@ -14,6 +27,35 @@ def debug_sever(target_vel_queue: Queue[tuple[float, float, float]]):
         target_vel_queue.put((data["x"], data["y"], data["w"]))
 
         return web.Response()
+
+    async def send_current_pose(ws: web.WebSocketResponse):
+        def get_current_pose() -> tuple[float, float, float]:
+            return current_pose.get()
+
+        while True:
+            x, y, w = await asyncio.to_thread(get_current_pose)
+            await ws.send_json(
+                {
+                    "x": x,
+                    "y": y,
+                    "w": w,
+                }
+            )
+
+    @routes.get("/pose")
+    async def pose(request: web.Request) -> web.WebSocketResponse:
+        ws = web.WebSocketResponse()
+        await ws.prepare(request)
+
+        task = asyncio.create_task(send_current_pose(ws))
+
+        async for msg in ws:
+            if msg.type == WSMsgType.ERROR:
+                print("ws connection closed with exception %s" % ws.exception())
+
+        task.cancel()
+
+        return ws
 
     app = web.Application()
     app.add_routes(routes)
