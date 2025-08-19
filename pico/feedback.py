807f5d712dcbbaff4023861d64b0ca50217b8a38
debater-coder <hamzah@syedahmed.net>
2025-08-19 16:19:55 +1000

    fix: better p control

--- pico/feedback.py
+++ pico/feedback.py
@@ -24,7 +24,6 @@ class Encoder:
         return str(self.odom)
 
 
-
 class MotorFeedback:
     """Motor using encoder as speed feedback. Direction is assumed from motor direction."""
 
@@ -34,7 +33,7 @@ class MotorFeedback:
         dir: int,
         encoder: int,
         pulses_per_revolution: int = 1050,
-        kp: float = 0.1,
+        kp: float = 10,
         ki: float = 0,
         kd: float = 0,
         reverse=False,
@@ -46,9 +45,9 @@ class MotorFeedback:
         self.ki = ki
         self.kd = kd
         self.reverse = reverse
-        self.speed = 0 # rev/s
-        self.target = 0 # rev/s
-        self.motor_power = 0 # [-100, 100]
+        self.speed = 0  # rev/s
+        self.target = 0  # rev/s
+        self.motor_power = 0  # [-100, 100]
         self.last_time = time.ticks_us()
         self.last_error = 0
         self.odom = 0.0
@@ -76,7 +75,9 @@ class MotorFeedback:
 
         # update motor
         self.motor.speed(self.motor_power * (-1 if self.reverse else 1))
-        self.encoder.direction = round(self.motor_power / max(abs(self.motor_power), 1e-6))
+        self.encoder.direction = round(
+            self.motor_power / max(abs(self.motor_power), 1e-6)
+        )
 
     def set_speed(self, speed):
         self.target = speed
