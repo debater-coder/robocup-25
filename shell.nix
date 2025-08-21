ef4d9b282133a445ab1ef163611cc03460133526
debater-coder <hamzah@syedahmed.net>
2025-08-22 09:03:49 +1000

    fix: cv libs

--- shell.nix
+++ shell.nix
@@ -4,6 +4,8 @@ mkShell {
   NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
     stdenv.cc.cc
     zlib
+    libglvnd
+    glib
   ];
   NIX_LD = lib.fileContents "${stdenv.cc}/nix-support/dynamic-linker";
   buildInputs = [ python312 ];
