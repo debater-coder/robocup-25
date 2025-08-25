8bed1323ba092a15c49b6e6a432e8d7d9d702a09
debater-coder <hamzah@syedahmed.net>
2025-08-25 13:41:01 +1000

    fix: correct gpio

--- test_motors.py
+++ test_motors.py
@@ -1,6 +1,6 @@
 from gpiozero import PhaseEnableMotor
 
-rl = PhaseEnableMotor(17, 4, pwm=True)
+rl = PhaseEnableMotor(13, 6, pwm=True)
 fl = PhaseEnableMotor(22, 27, pwm=True)
-rr = PhaseEnableMotor(9, 10, pwm=True)
+rr = PhaseEnableMotor(10, 9, pwm=True)
 fr = PhaseEnableMotor(11, 5, pwm=True)
